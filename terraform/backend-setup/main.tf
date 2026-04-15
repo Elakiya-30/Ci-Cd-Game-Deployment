aa2cf3637c43a9506fcff8abaa668d3c8bc7171e
Elakiya-30 <elakiyachezhiyan30@gmail.com>
2026-04-15 08:41:28 +0530

    Remove Terraform version and provider configurations
    Removed Terraform version and provider specifications.

--- terraform/backend-setup/main.tf
+++ terraform/backend-setup/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "my-game-terraform-state-bucket-elakiya-123"
 
